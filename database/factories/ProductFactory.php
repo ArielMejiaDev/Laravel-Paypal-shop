754d990020dfca9bc5585e1ae1d44d27dc332fde
ArielSalvadorDev <arielsalvadormejia@gmail.com>
2019-09-30 08:03:19 -0600

    better views and product names fixes

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 $factory->define(Product::class, function (Faker $faker) {
     $prices = [25.00, 50.00, 75.00, 100.00];
     return [
-        'name'  =>  "T-shirt{$faker->city}",
+        'name'  =>  "T-shirt {$faker->city}",
         'price' =>  $prices[rand(0, 3)],
     ];
 });
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,20 +21,25 @@
         @endif
         <div class="row">
             @foreach($products as $product)
-                <div class="col-md-3">
+                <div class="col-md-3 my-3">
                     <div class="card">
                         <div class="card-header">
                             <!-- The product name (duh..) -->
                             {{ $product->name }}
                         </div>
                         <div class="card-body">
+                            <img 
+                                src="https://images.unsplash.com/photo-1564584217132-2271feaeb3c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" 
+                                alt="T-shirt" 
+                                class="img-fluid rounded mb-2"
+                            >
                             <h3>
                                 <!-- We format the number to a price with currency behind it -->
                                 {{ number_format($product->price, 2) }} USD
                             </h3>
                             <a href="{{ route('add', [ $product->getRouteKey() ]) }}">
                                 <!-- The button for adding the product to the cart -->
-                                <button class="btn btn-primary">Add to cart</button>
+                                <button class="btn btn-primary btn-block">Add to cart</button>
                             </a>
                         </div>
                     </div>
